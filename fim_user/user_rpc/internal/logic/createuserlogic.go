4e4d717fb987f4b3aab1c9d76aebc5fb856a02fa
Richard Castle <castle@RicharddeMacBook-Air.local>
2024-07-10 15:44:10 +0800

    day4-v2: 1.4.2

---
+++ fim_user/user_rpc/internal/logic/createuserlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"fim/fim_user/user_rpc/internal/svc"
+	"fim/fim_user/user_rpc/types/user_grpc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CreateUserLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreateUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateUserLogic {
+	return &CreateUserLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CreateUserLogic) CreateUser(in *user_grpc.UserCreateRequest) (*user_grpc.UserCreateResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user_grpc.UserCreateResponse{}, nil
+}
---
+++ fim_user/user_rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "fim/fim_user/user_rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
