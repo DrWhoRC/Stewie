e57602ece77af10c760c74ac90822e55910898ce
Richard Castle <castle@RicharddeMacBook-Air.local>
2024-07-12 01:08:33 +0800

    day5-v2: 1.5.2

--- fim_gateway/gateway.go
+++ fim_gateway/gateway.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"regexp"
 	"strings"
@@ -35,8 +37,23 @@ func gateway(res http.ResponseWriter, req *http.Request) {
 	}
 
 	url := fmt.Sprintf("%s/%s", addr, req.URL.String())
+
+	//先将请求体读取到body中，此时req.Body已经被读取过一次，所以要重新设置
+	body, err := ioutil.ReadAll(req.Body)
+	//随后使用NopCloser方法再次设置req.Body，req.body来设置proxybody，body来获取contentlength
+	req.Body = ioutil.NopCloser(bytes.NewReader(body))
+
 	proxyReq, _ := http.NewRequest(req.Method, url, req.Body)
-	fmt.Println(req.Header, "\n", req.Body)
+	fmt.Println(req.Header, "\n", proxyReq.Body)
+
+	proxyReq.ContentLength = int64(len(body))
+	//罪魁祸首！！！！！！！！！！！！！
+	//就是上边那一行代码！！！！！！！！
+	//header中的contentlength确实是56，这也就解释了为什么打印的header信息中没有错误
+	//但是！！！proxyReq.ContentLength和header中的contentlength不是一个东西，
+	//所以要手动设置proxyReq.ContentLength的长度！
+
+	proxyReq.Header.Set("Content-Type", "application/json")
 
 	for name, values := range req.Header {
 		for _, value := range values {
@@ -54,7 +71,6 @@ func gateway(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	io.Copy(res, response.Body)
-
 }
 
 var configFile = flag.String("f", "settings.yaml", "The Config File")
