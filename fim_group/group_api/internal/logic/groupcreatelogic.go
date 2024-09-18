3e8623dcc18676e5606fdaf803f8d6b6b2522523
Richard Castle <castle@RicharddeMacBook-Air.local>
2024-09-18 20:22:40 +0200

    day 31, 4 days to die

--- fim_group/group_api/internal/logic/groupcreatelogic.go
+++ fim_group/group_api/internal/logic/groupcreatelogic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"strings"
 
 	"fim/fim_group/group_api/internal/svc"
 	"fim/fim_group/group_api/internal/types"
@@ -45,6 +46,7 @@ func (l *GroupCreateLogic) GroupCreate(req *types.GroupCreateRequest) (resp *typ
 		group.Title = req.Name
 		group.Size = int8(req.Size)
 		group.IsSearchable = req.IsSearch
+		group.Avatar = "fim_file/file_api/uploads/avatar/IMG_7525.jpeg"
 
 	case 2: //选人创建
 		if len(req.UserIdList) == 0 {
@@ -53,6 +55,7 @@ func (l *GroupCreateLogic) GroupCreate(req *types.GroupCreateRequest) (resp *typ
 		var userIdList = []uint32{
 			uint32(req.UserId), //先把自己加进去
 		}
+		var nameList []string
 		for _, v := range req.UserIdList {
 			userIdList = append(userIdList, uint32(v))
 			res, err := l.svcCtx.UserRpc.UserInfo(context.Background(), &user_grpc.UserInfoRequest{
@@ -61,14 +64,31 @@ func (l *GroupCreateLogic) GroupCreate(req *types.GroupCreateRequest) (resp *typ
 			if err != nil {
 				return nil, err
 			}
+
 			user := usermodel.UserModel{}
 			err = json.Unmarshal(res.Data, &user)
 			if err != nil {
 				return nil, errors.New("json.Unmarshal(res.Data,&user) error")
 			}
-		}
 
+			if len(strings.Join(nameList, "、")) >= 32 {
+				break
+			}
+			nameList = append(nameList, user.NickName)
+		}
+		nameList[len(nameList)-1] = nameList[len(nameList)-1] + "的群聊"
+		group.Title = strings.Join(nameList, "、")
+		group.Avatar = "fim_file/file_api/uploads/avatar/IMG_7525.jpeg"
+		group.IsSearchable = req.IsSearch
 	}
 
-	return
+	err = l.svcCtx.DB.Create(&group).Error
+	if err != nil {
+		logx.Error(err)
+		return nil, err
+	}
+
+	return &types.GroupCreateResponse{
+		GroupId: group.ID,
+	}, nil
 }
